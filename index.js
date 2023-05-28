3a88938c78a585cb7d339be7bee9eb6bdde0a13d
Michael Mason <m.masonn3@gmail.com>
2023-05-28 12:37:57 -0400

    loop done

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+let thankyoumessage= []
+function writeCards(names){
+  for ( let i = 0; i <names.length; i++) {
+    console.log(`thank you ${names[i]}`);
+thankyoumessage.push(`Thank you, ${names[i]}, for the wonderful surprise gift!` )
+}
+console.log(thankyoumessage)
+return thankyoumessage
+}
+function countDown(number){
+    while( number > -1 )
+console.log(number--)
+}
